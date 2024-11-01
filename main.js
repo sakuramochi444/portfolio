f5822b41e3a5247bcc2b30403174cf98f39dcd30
sakuramochi444 <0404taichi8@gmail.com>
2024-11-01 09:16:20 +0900

    edit

---
+++ main.js
@@ -0,0 +1,16 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        
+        const headerOffset = 100; // ヘッダーの高さに合わせて調整
+        const elementPosition = targetElement.getBoundingClientRect().top;
+        const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: 'smooth'
+        });
+    });
+});
